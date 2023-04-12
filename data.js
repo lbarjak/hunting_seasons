3069cf958bd9e261aae05e04766261d0b15d0ebd
lbarjak <barjak.laszlo@gmail.com>
2023-04-12 22:57:46 +0200

    random answers sequence

--- data.js
+++ data.js
@@ -53,4 +53,14 @@ let answers = () => {
     return [...new Set(answers)]
 }
 
-let hunting_seasons = answers()
+const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+}
+
+let hunting_seasons = shuffleArray(answers())
